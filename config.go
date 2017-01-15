0742550df7f2c6c5645666241f67a86af15c9147
puzanov <puzanov@gmail.com>
2017-01-15 19:42:05 +0600

    + added main logic and tests

--- config.go
+++ config.go
@@ -1,1 +1,27 @@
 package simple_config
+
+import (
+	"github.com/spf13/viper"
+	"log"
+)
+
+type SimpleConfig struct {
+	configFile string
+	configType string
+}
+
+func (c *SimpleConfig) Get(key string) interface{} {
+	return viper.Get(key)
+}
+
+func NewSimpleConfig(configFile string, configType string) SimpleConfig {
+	config := SimpleConfig{configFile, configType}
+	viper.SetConfigName(config.configFile)
+	viper.SetConfigType(config.configType)
+	viper.AddConfigPath("./")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Fatal configuration error: %s \n", err)
+	}
+	return config
+}
--- config_test.go
+++ config_test.go
@@ -1,1 +1,18 @@
 package simple_config
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConfigGet_Exist(t *testing.T) {
+	config := NewSimpleConfig("./config.test", "yml")
+	value := config.Get("test-key")
+	assert.Equal(t, value, "test value")
+}
+
+func TestConfigGet_NonExist(t *testing.T) {
+	config := NewSimpleConfig("./config.test", "yml")
+	value := config.Get("allauakbarrr")
+	assert.Nil(t, value)
+}
